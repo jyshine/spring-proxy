3644be4639232a54e16f3d0e46ba739519de9810
신준영 <jyshine3@gmail.com>
2022-01-07 22:28:35 +0900

    feat: proxy pattern test code

---
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -0,0 +1,34 @@
+package hello.proxy.pureproxy.proxy;
+
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import org.junit.jupiter.api.Test;
+import sun.tools.jconsole.ProxyClient;
+
+public class ProxyPatternTest {
+
+    @Test
+    void noProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+
+        client.execute();
+        client.execute();
+        client.execute();
+
+    }
+
+    @Test
+    void cacheProxyTest(){
+        RealSubject subject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(subject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+
+        client.execute();
+        client.execute();
+        client.execute();
+    }
+
+
+}
